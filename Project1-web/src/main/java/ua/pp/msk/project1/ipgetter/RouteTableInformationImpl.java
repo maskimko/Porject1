13334fc936ac15a540e82497c0103a5323f9e26e
maskimko <maskimko@ukr.net>
2014-10-05 22:29:56 +0300

    End of working day. Nothing works. Written routes parser

--- Project1-web/src/main/java/ua/pp/msk/project1/ipgetter/RouteTableInformationImpl.java
+++ Project1-web/src/main/java/ua/pp/msk/project1/ipgetter/RouteTableInformationImpl.java
@@ -6,49 +6,79 @@
 package ua.pp.msk.project1.ipgetter;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.xml.bind.DatatypeConverter;
+import org.apache.log4j.Logger;
 
 /**
  *
  * @author maskimko
  */
-public class RouteTableInformationImpl implements RouteTableInformation{
+public class RouteTableInformationImpl implements RouteTableInformation {
 
-    
+    public static final String LINUXROUTE = "/proc/net/route";
+    public static final String LINUXARP = "/proc/net/route";
+    public static final String ROUTEPATTERNLINE = "(\\w+\\d+)\\s+([0-9A-F]{8})\\s+([0-9A-F]{8})\\s+([0-9A-F]{4})\\s+(\\d+)\\s+(\\d+)\\s+(\\d+)\\s+([0-9A-F]{8})\\s+(\\d+)\\s+(\\d+)\\s+(\\d+)";
+
     @Override
-    public List<RouteTableRecord> getRoutes() {
-
-        List<RouteTableRecord> routes = new ArrayList<RouteTableRecord>();
+    public List<RouteTableLinuxRecord> getRoutes() {
+        List<String> routeLines = new ArrayList<String>();
+        List<RouteTableLinuxRecord> routes = new ArrayList<RouteTableLinuxRecord>();
+        Pattern routePattern = Pattern.compile(ROUTEPATTERNLINE);
+        Matcher routeMatcher = null;
         try {
-            
-            Process exec = Runtime.getRuntime().exec("route -n");
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(exec.getInputStream()));
-            String routeLine = null;
-            do {
-                routeLine = bufferedReader.readLine();
-                String[] routeFields =routeLine.split("\\s*");
-                
-            } while (routeLine != null);
+            File route = new File(LINUXROUTE);
+            BufferedReader bf = new BufferedReader(new FileReader(route));
+            String currentLine;
+            if (route.canRead()) {
+                while ((currentLine = bf.readLine()) != null) {
+
+                   routes.add(getRouteRecordFromString(currentLine, routePattern));
+
+                }
+
+            } else {
+                Logger.getLogger(GetIpList.class.getName()).error("Cannot read file " + LINUXROUTE);
+            }
         } catch (IOException ex) {
-            java.util.logging.Logger.getLogger(GetIpList.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(GetIpList.class.getName()).error("Cannot get information from file " + LINUXROUTE);
         }
         return routes;
- 
+
     }
-    
-    private RouteTableRecord getRecord(String[] fields) throws UnknownHostException{
-        RouteTableRecord record = new RouteTableRecordImpl();
-        record.setDestinationInetAddress((Inet4Address) InetAddress.getByName(fields[0]));
-        record.setGatewayInetAddress(InetAddress.getByName(fields[1]));
-        
+
+    private byte[] getByteArray(String input) {
+        byte[] byteArray;
+        byteArray = DatatypeConverter.parseHexBinary(input);
+        return byteArray;
     }
-    
+
+    public RouteTableLinuxRecord getRouteRecordFromString(String line, Pattern routePattern) throws UnknownHostException {
+        Matcher routeMatcher = routePattern.matcher(line);
+        RouteTableLinuxRecord routeRecord = null;
+        if (routeMatcher.matches()) {
+            routeRecord = new RouteTableRecordImpl();
+            routeRecord.setIfName(routeMatcher.group(1));
+            routeRecord.setDestinationInetAddress((Inet4Address) Inet4Address.getByAddress(getByteArray(routeMatcher.group(2))));
+            routeRecord.setGatewayInetAddress((Inet4Address) Inet4Address.getByAddress(getByteArray(routeMatcher.group(3))));
+            routeRecord.setGenmaskInetAddress((Inet4Address) Inet4Address.getByAddress(getByteArray(routeMatcher.group(8))));
+            routeRecord.setFlags(Integer.decode(routeMatcher.group(4)));
+            routeRecord.setReferences(Integer.parseInt(routeMatcher.group(5)));
+            routeRecord.setUses(Integer.parseInt(routeMatcher.group(6)));
+            routeRecord.setMetric(Integer.parseInt(routeMatcher.group(7)));
+            routeRecord.setMtu(Integer.parseInt(routeMatcher.group(9)));
+            routeRecord.setWindow(Integer.parseInt(routeMatcher.group(10)));
+            routeRecord.serIrtt(Integer.parseInt(routeMatcher.group(11)));
+        }
+        return routeRecord;
+    }
 
 }
